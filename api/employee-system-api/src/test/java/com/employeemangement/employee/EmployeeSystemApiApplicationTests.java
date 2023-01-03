4797260be3620e76f4f22a14c8e1a2d8157352fc
JonathanMasih <j24masi@siena.edu>
2023-01-03 18:20:27 -0500

    first commit

---
+++ api/employee-system-api/src/test/java/com/employeemangement/employee/EmployeeSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeemangement.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
