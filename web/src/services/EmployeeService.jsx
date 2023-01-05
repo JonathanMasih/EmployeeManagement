82a7fd7f9ac2469f60c3e54b39c54988b5e52d92
JonathanMasih <j24masi@siena.edu>
2023-01-04 19:18:51 -0500

    add empolyee working

---
+++ web/src/services/EmployeeService.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const EMPLOYEE_API_BASE = "http://localhost:8081/api/v1/employees";
+
+
+class EmployeeService{
+
+    saveEmployee(employee){
+        return axios.post(EMPLOYEE_API_BASE,employee);
+    }
+
+}
+
+export default new EmployeeService();
