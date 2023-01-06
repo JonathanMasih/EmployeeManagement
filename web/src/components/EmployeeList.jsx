4950f32197e118688f0a92b9d0b2522a011aa3f6
JonathanMasih <j24masi@siena.edu>
2023-01-06 15:56:16 -0500

    Added employee componenet to render employees

--- web/src/components/EmployeeList.jsx
+++ web/src/components/EmployeeList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState,useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import EmployeeService from '../services/EmployeeService';
+import Employee from './Employee';
 
 export const EmployeeList = () => {
   const navigate = useNavigate();
@@ -47,38 +48,11 @@ export const EmployeeList = () => {
             <tbody className="bg-white">
               {employees.map((employee) =>{
                 return(
-                  <tr>
-                  <td className="text-left px-6 py-4 whitespace-nowrap">
-                      <div className="text-sm text-gray-500">
-                       {employee.firstName}
-                       </div>
-                  </td>
-                  <td className="text-left px-6 py-4 whitespace-nowrap">
-                      <div className="text-sm text-gray-500">
-                       {employee.lastName}
-                       </div>
-                  </td>
-                  <td className="text-left px-6 py-4 whitespace-nowrap">
-                      <div className="text-sm text-gray-500">
-                       {employee.emailId}
-                       </div>
-                  </td>
-                  <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
-                      <a 
-                        href="#"
-                        className="text-indigo-600 hover:text-indigo-800 px-4">
-                          Edit
-                      </a>
-                      <a  
-                        href="#"
-                        className="text-indigo-600 hover:text-indigo-800">
-                          Delete
-                      </a>
-                  </td>
-                </tr>
+                  <Employee key ={employee.id} employee = {employee} > </Employee> 
                 )
               })}
-            </tbody>)}
+            </tbody>
+            )}
           </table>
 
 
